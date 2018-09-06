87812e65a936f61fa4cdd6f34fd31768e40db396
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-09-06 11:14:45 +0200

    DONE! now for testing

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -1,55 +1,21 @@
 let assert = require('assert');
-let tester = require('../greetings-factory');
+let factory = require('../greetings-factory');
+let data = require('../dataBase');
 
-const theGreetingsVar = tester();
+const theGreetingsFac = factory();
+const theGreetingsData = data();
+
 describe('the greetings function basic test', function () {
     it('greetings function test greeting in English', function () {
 
-        assert.equal(theGreetingsVar.greet('Richard', "english"), "HELLO RICHARD");
+        assert.equal(theGreetingsFac.greet('Richard', "english"), "HELLO RICHARD");
     });
     it('greetings function test greeting in Afrikaans', function () {
 
-        assert.equal(theGreetingsVar.greet('Richard', "afrikaans"), "GOEIE DAG RICHARD");
+        assert.equal(theGreetingsFac.greet('Richard', "afrikaans"), "GOEIE DAG RICHARD");
     });
     it('greetings function test greeting in isiXhosa', function () {
 
-        assert.equal(theGreetingsVar.greet('Richard', "isiXhosa"), "USUKU OLUMNWANDI RICHARD");
-    });
-});
-describe('the greetings function testing the counter', function () {
-    it('greetings function should test is incremented by 1', function () {
-
-        theGreetingsVar([{
-            'Richard': 0
-        }]);
-
-        assert.equal(theGreetingsVar.TheGreetCounter(), 1);
-    });
-    it('greetings function should test if the same name should stay one', function () {
-
-        theGreetingsVar([{
-            'Richard': 0
-        }, ]);
-
-        assert.equal(theGreetingsVar.TheGreetCounter(), 1);
-
-        theGreetingsVar([{
-            'Richard': 0
-        }, ]);
-
-        assert.equal(theGreetingsVar.TheGreetCounter(), 1);
-
-    });
-    it('greetings function should if the it has three different names, should return three', function () {
-
-        theGreetingsVar([{
-            'Aya': 0
-        }, {
-            'mike': 0
-        }, {
-            'Sibo': 0
-        }]);
-
-        assert.equal(theGreetingsVar.TheGreetCounter(), 3);
+        assert.equal(theGreetingsFac.greet('Richard', "isiXhosa"), "USUKU OLUMNWANDI RICHARD");
     });
 });
