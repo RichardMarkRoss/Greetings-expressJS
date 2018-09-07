e36887e2c205bd05935052cf9db988c85fa12c85
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-09-07 09:50:39 +0200

    changes name in the data base to uppercase so that name cannot be repeated

--- dataBase.js
+++ dataBase.js
@@ -1,11 +1,12 @@
 module.exports = function (pool) {
     async function dataHeld (holdName) {
-        let user = await pool.query('select * from hold_name where names = $1', [holdName]);
+        let nameHeld = holdName.toUpperCase();
+        let user = await pool.query('select * from hold_name where names = $1', [nameHeld]);
         if (user.rows.length !== 0) {
             let holdInt = user.rows[0].counter + 1;
-            await pool.query('update hold_name set counter = $1 where names = $2', [holdInt, holdName]);
+            await pool.query('update hold_name set counter = $1 where names = $2', [holdInt, nameHeld]);
         } else {
-            await pool.query('insert into hold_name (names, counter) values ($1, $2)', [holdName, 1]);
+            await pool.query('insert into hold_name (names, counter) values ($1, $2)', [nameHeld, 1]);
         }
     };
     async function TheGreetCounter () {
